c93187fb711e60402f275cf692b41577f9934921
oskar194 <worminion@gmail.com>
2017-05-10 13:50:57 +0200

    Oddane zadanie, wszystko dziala

--- Z3/TablicaEvents/TablicaEvents/Program.cs
+++ Z3/TablicaEvents/TablicaEvents/Program.cs
@@ -4,12 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TablicaEvents
+namespace Tablica
 {
     class Program
     {
+		public static void WhenElementAdded(object sender, EventArgs e) {
+			Console.WriteLine("Dodano element!");
+		}
+		public static void WhenSizeChanged(object sender, EventArgs e) {
+			Console.WriteLine("Zmieniono wielkosc do: " + ((SizeArgs)e).Val);
+		}
+
         static void Main(string[] args)
         {
+            Table t = new Table(5);
+			t.elementAdded += WhenElementAdded;
+			t.sizeChange += WhenSizeChanged;
+            t.Add(0);
+            t.Add(1);
+            t.Add(2);
+            t.Add(3);
+            t.Add(4);
+
+            t.Add(5);
+            t.Add(6);
+            t.Add(7);
+            //t[5] = 6;
+            t[12] = 8;
+            t.Add(9);
+            for (int i = 0; i < 13; i++)
+            {
+                Console.WriteLine(t[i].ToString());
+
+            }
+
+            Console.ReadKey();
+
         }
-    }
+	}
 }
---
+++ Z3/TablicaEvents/TablicaEvents/SizeArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tablica {
+	public class SizeArgs : EventArgs {
+		int val;
+		public SizeArgs(int val) {
+			this.Val = val;
+		}
+
+		public int Val {
+			get {
+				return val;
+			}
+
+			set {
+				val = value;
+			}
+		}
+	}
+}
