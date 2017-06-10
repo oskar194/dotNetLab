f8e71e046e55ef3a418cf69b72df7cbd7fb00c2c
oskar194 <worminion@gmail.com>
2017-06-10 21:20:07 +0200

    Zadanie skonczone

--- Z4/TablicaMultithread/TablicaMultithread/Program.cs
+++ Z4/TablicaMultithread/TablicaMultithread/Program.cs
@@ -2,11 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TablicaMultithread {
+	public class BlockingThread {
+		public void Run(Table tab) {
+			Console.WriteLine("BlockingThread adding " + Thread.CurrentThread.ManagedThreadId);
+			//if (new Random().Next(100) > 20) {
+			//	Console.WriteLine("Waiting! " + Thread.CurrentThread.ManagedThreadId);
+			//	Thread.Sleep(10000);
+			//}
+			tab.AddBlocking(Thread.CurrentThread.ManagedThreadId);
+		}
+	}
+
+
+	public class NonBlockingThread {
+		public void Run(Table tab) {
+			Console.WriteLine("NonBlokingThread try to add " + Thread.CurrentThread.ManagedThreadId);
+			tab.AddNonblocking(Thread.CurrentThread.ManagedThreadId);
+		}
+	}
+
 	class Program {
 		static void Main(string[] args) {
+			Table tab = new Table(5);
+			List<Thread> tList = new List<Thread>();
+			for (int i = 1; i <= 100; i++) {
+				if (i > 1) {
+					tList.Add(new Thread(() => new BlockingThread().Run(tab)));
+				} else {
+					//tList.Add(new Thread(() => new NonBlockingThread(new Random().Next(100)).Run(tab)));
+
+				}
+			}
+			foreach(Thread t in tList) {
+				t.Start();
+			}
+			foreach (Thread t in tList) {
+				t.Join();
+			}
+			for (int i = 0; i < tab.Length; i++) {
+				Console.WriteLine(tab[i]);
+			}
+			Console.WriteLine("Table length is : " + tab.Length);
+			Console.ReadKey();
 		}
 	}
 }
--- Z4/TablicaMultithread/TablicaMultithread/Table.cs
+++ Z4/TablicaMultithread/TablicaMultithread/Table.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TablicaMultithread {
-	class Table {
-		int[] tab;
-		int last;
+	public class Table {
+		private int[] tab;
+		private int last;
+		private object syncToken;
+		private int length;
+		public int Length {
+			get{
+				return length;
+			}
+		}
 		public event EventHandler sizeChange;
 		public event EventHandler elementAdded;
 
 		public Table(int size) {
 			this.tab = new int[size];
 			this.last = 0;
+			length = size;
+			syncToken = new object();
 		}
 
 		protected virtual void OnAddition(EventArgs e) {
@@ -31,33 +41,38 @@ namespace TablicaMultithread {
 				tab = new int[tab.Length * 2];
 				OnSizeChange(new SizeArgs(tab.Length));
 				tab2.CopyTo(tab, 0);
-
 				// tab[last] = x;
 				// last++;
 			}
 			tab[last] = x;
+			length = tab.Length;
 			OnAddition(EventArgs.Empty);
 			last++;
 		}
 
 		public void AddBlocking(int x) {
-			lock (tab) {
-				if (last == tab.Length - 1) {
-					int[] tab2 = new int[tab.Length];
-					tab.CopyTo(tab2, 0);
-					tab = new int[tab.Length * 2];
-					OnSizeChange(new SizeArgs(tab.Length));
-					tab2.CopyTo(tab, 0);
-				}
-				tab[last] = x;
-				OnAddition(EventArgs.Empty);
-				last++;
+			int timeBefore = DateTime.Now.Millisecond;
+			lock (syncToken) {
+				Add(x);
 			}
+			Console.WriteLine("Thread " + Thread.CurrentThread.ManagedThreadId + " waited: " + (DateTime.Now.Millisecond - timeBefore) + "ms");
 		}
 
 		public void AddNonblocking(int x) {
-
+			var lockAquired = Monitor.TryEnter(syncToken);
+			if (!lockAquired) {
+				Console.Write(" Tab is locked. Exiting ");
+				//Monitor.Exit(syncToken);
+				return;
+			} else {
+				Add(x);
+				Console.Write("Value added" + '\n');
+				Monitor.Exit(syncToken);
+			}
 		}
+
+
+		
 
 		public int this[int index] {
 			get {
@@ -76,6 +91,7 @@ namespace TablicaMultithread {
 					tab2.CopyTo(tab, 0);
 				}
 				tab[index] = value;
+				length = tab.Length;
 				OnAddition(EventArgs.Empty);
 				last = index;
 			}
