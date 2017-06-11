ce897b0de4ffd967a222de515f868bc10b31a003
oskar194 <worminion@gmail.com>
2017-06-11 03:46:34 +0200

    Dodalem w kliencie UI i watek do progressBara

--- Z5/ClientServer/ClientProgram/Form1.Designer.cs
+++ Z5/ClientServer/ClientProgram/Form1.Designer.cs
@@ -23,12 +23,120 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.ipLabel = new System.Windows.Forms.Label();
+			this.portLabel = new System.Windows.Forms.Label();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// ipLabel
+			// 
+			this.ipLabel.AutoSize = true;
+			this.ipLabel.Location = new System.Drawing.Point(12, 53);
+			this.ipLabel.Name = "ipLabel";
+			this.ipLabel.Size = new System.Drawing.Size(17, 13);
+			this.ipLabel.TabIndex = 0;
+			this.ipLabel.Text = "IP";
+			// 
+			// portLabel
+			// 
+			this.portLabel.AutoSize = true;
+			this.portLabel.Location = new System.Drawing.Point(12, 83);
+			this.portLabel.Name = "portLabel";
+			this.portLabel.Size = new System.Drawing.Size(26, 13);
+			this.portLabel.TabIndex = 1;
+			this.portLabel.Text = "Port";
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(12, 12);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(260, 23);
+			this.progressBar1.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(197, 149);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Browse";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 215);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(260, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "SEND";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(68, 53);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(0, 13);
+			this.label3.TabIndex = 5;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(53, 53);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 6;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(53, 83);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 7;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(15, 152);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(176, 20);
+			this.textBox3.TabIndex = 8;
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.portLabel);
+			this.Controls.Add(this.ipLabel);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label ipLabel;
+		private System.Windows.Forms.Label portLabel;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
 	}
 }
 
--- Z5/ClientServer/ClientProgram/Form1.cs
+++ Z5/ClientServer/ClientProgram/Form1.cs
@@ -5,13 +5,43 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ClientProgram {
+
 	public partial class Form1 : Form {
 		public Form1() {
+
 			InitializeComponent();
+			Thread t = new Thread(delegate () {
+				for (;;) {
+					this.Invoke((MethodInvoker)delegate () {
+						if (progressBar1.Value == progressBar1.Maximum) {
+							progressBar1.Value = 0;
+						}
+						progressBar1.Value++;
+					});
+				}
+			});
+			t.Start();
+
+		}
+
+		public void UpdateProgressBar() {
+
+				if (this.InvokeRequired) {
+					this.BeginInvoke(new Action(UpdateProgressBar));
+					return;
+				}
+				if (progressBar1.Value == progressBar1.Maximum) {
+					progressBar1.Value = 0;
+				}
+				progressBar1.PerformStep();
+				Thread.Sleep(1000);
+			
+
 		}
 	}
 }
